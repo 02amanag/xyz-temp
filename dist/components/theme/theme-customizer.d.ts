042a898904ef1c5b45bcce2980437ada13eb08d8
Aman Agarwal <aman.agarwal@fiftyfivetech.io>
2025-07-03 18:17:42 +0530

    update dist

--- dist/components/theme/theme-customizer.d.ts
+++
@@ -1,6 +0,0 @@
-interface ThemeCustomizerProps {
-    className?: string;
-}
-export declare function ThemeCustomizer({ className }: ThemeCustomizerProps): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=theme-customizer.d.ts.map
