eb7cd245af01f31c37dc06c6e3449e546c8f3ae2
Aman Agarwal <aman.agarwal@fiftyfivetech.io>
2025-07-03 14:11:06 +0530

    inital commit

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ export { ThemeProvider, useTheme } from "./components/theme/theme-provider";
 export { useIsMobile } from "./hooks/use-mobile";
 export { useMediaQuery } from "./hooks/use-media-query";
 
+// Add more exports as needed for other standalone components/hooks
+
 // Note: The following components require complex setup and are not exported:
 // - AppSidebar (requires @/schema/menu, @/hooks/use-config, etc.)
 // - Layout (requires @/lib/utils, @/hooks/use-config, etc.)
